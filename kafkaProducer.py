fea27ad8435d343ffbf11bc87af9349f94b960f9
mexudi <soufianmexudi20@gmail.com>
2023-02-19 11:59:18 +0100

    add tweets listener

--- kafkaProducer.py
+++ kafkaProducer.py
@@ -25,3 +25,22 @@ def twitter_auth():
     api = tweepy.API(authenticate, wait_onrate_limit=True)
     return api
 
+
+class TweetListener(tweepy.Stream):
+
+    def on_date(self, raw_data):
+        logging.info(raw_data)
+        producer.send(topic_name, value=raw_data)
+        return True
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            # returning False in on_data disconnects the stream
+            return False
+    def start_streaming_tweets(self, search term):
+        self.fillter(track=search_term, stall_warning=True, language['en']
+
+if __name__ == '__main__':
+    twitter_stream = TweetListner(consumerKey, consumerSecret, accessToken, accessTokenSecret)
+    twitter_stream.start_streaming_tweets(search_term)
+
