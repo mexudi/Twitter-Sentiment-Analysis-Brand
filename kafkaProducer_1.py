bffea465412eff70dd322ef39a2a9b3a43811b49
mexudi <soufianmexudi20@gmail.com>
2023-03-14 11:55:34 +0100

    add kafka producer with snscrap

---
+++ kafkaProducer_1.py
@@ -0,0 +1,13 @@
+from kafka import KafkaProducer
+
+# Set up the Kafka producer
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+import snscrape.modules.twitter as sntwitter
+
+# Set up the search criteria
+search_terms = 'python'
+
+# Fetch the tweets and send them to Kafka
+for tweet in sntwitter.TwitterSearchScraper(search_terms).get_items():
+    producer.send('twitter', tweet.content.encode('utf-8'))
+
