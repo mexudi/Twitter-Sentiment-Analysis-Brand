d745f53250f4191919d1f4a441a0b63c59dfe8df
mexudi <soufianmexudi20@gmail.com>
2023-03-17 01:28:17 +0100

    update the producer

--- kafkaProducer.py
+++ kafkaProducer.py
@@ -1,46 +1,30 @@
-#Import libraries
-
-import auth_tokens as auth
-import tweepy
-import logging
-
-
 from kafka import KafkaProducer
+import snscrape.modules.twitter as sntwitter
+import json
 
-#Generate kafka producer / localhost and 9092 default ports
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092'],
+    value_serializer=lambda m: json.dumps(m).encode('utf-8'))
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
-search_term = 'elon musk'
+# Twitter keyword search
+keyword = "MXSoufian"
 topic_name = 'TW_ANALYSIS'
+for i, tweet in enumerate(sntwitter.TwitterSearchScraper(f"from:{keyword}").get_items()):
+    if i > 1:
+        break
+    tweet_dict = {
+        "id": tweet.id,
+        "date": tweet.date.strftime('%Y-%m-%d %H:%M:%S'),
+        "content": tweet.rawContent,
+        "username": tweet.user.username,
+        "replyCount": tweet.replyCount,
+        "retweetCount": tweet.retweetCount,
+        "likeCount": tweet.likeCount
+    }
+    # Send the tweet data to Kafka topic
+    producer.send(topic_name, value=tweet_dict)
+    print(f"Sent tweet with id {tweet.id} to Kafka")
 
+# Wait for any outstanding messages to be delivered and delivery reports received
+producer.flush()
 
-def twitterAuth():
-    # create the authentication object
-    authenticate = tweepy.OAuthHandler(auth.consumer_keys, auth.consumer_secret)
-    # set the access token and the access token secret
-    authenticate.set_access_token(auth.access_token, auth.access_secret)
-    # create the API object
-    api = tweepy.API(authenticate, wait_on_rate_limit=True)
-    return api
-
-
-class TweetListener(tweepy.Stream):
-
-    def on_data(self, raw_data):
-        logging.info(raw_data)
-        producer.send(topic_name, value=raw_data)
-        return True
-
-    def on_error(self, status_code):
-        if status_code == 420:
-            # returning False in on_data disconnects the stream
-            return False
-
-    def start_streaming_tweets(self, search_term):
-        self.filter(track=search_term, stall_warnings=True, languages=["en"])
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    twitter_stream = TweetListener(auth.consumer_keys, auth.consumer_secret, auth.access_token, auth.access_secret)
-    twitter_stream.start_streaming_tweets(search_term)
