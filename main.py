38838c97f43acee5358b4746abe31d8341aa4ade
mexudi <soufianmexudi20@gmail.com>
2023-03-17 01:48:12 +0100

    add text cleaning function

--- main.py
+++ main.py
@@ -10,3 +10,28 @@ from textblob import TextBlob
 import findspark
 findspark.init()
 
+# Text cleaning function
+def cleanTweet(tweet: str) -> str:
+    tweet = re.sub(r'http\S+', '', str(tweet))
+    tweet = re.sub(r'bit.ly/\S+', '', str(tweet))
+    tweet = tweet.strip('[link]')
+
+    # Remove users
+    tweet = re.sub('(RT\s@[A-Za-z]+[A-Za-z0-9-_]+)', '', str(tweet))
+    tweet = re.sub('(@[A-Za-z]+[A-Za-z0-9-_]+)', '', str(tweet))
+
+    # Remove punctuation
+    my_punctuation = '!"$%&\'()*+,-./:;<=>?[\\]^_`{|}~•@â'
+    tweet = re.sub('[' + my_punctuation + ']+', ' ', str(tweet))
+
+    # Remove numbers
+    tweet = re.sub('([0-9]+)', '', str(tweet))
+
+    # Remove hashtags
+    tweet = re.sub('(#[A-Za-z]+[A-Za-z0-9-_]+)', '', str(tweet))
+
+    # Remove extra simbols
+    tweet = re.sub('@\w+', '', str(tweet))
+    tweet = re.sub('\n', '', str(tweet))
+
+    return tweet
